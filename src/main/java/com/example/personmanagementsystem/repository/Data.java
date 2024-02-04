09aba477760edfddc033772a96c4f2c71d6c896d
Neda bashiri <neda.bashiri.dev@gmail.com>
2024-02-04 13:53:26 +0330

    Create data for repository.

---
+++ src/main/java/com/example/personmanagementsystem/repository/Data.java
@@ -0,0 +1,12 @@
+package com.example.personmanagementsystem.repository;
+
+import com.example.personmanagementsystem.entity.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Data {
+
+    public static List<Person> list = new ArrayList<>();
+
+}
